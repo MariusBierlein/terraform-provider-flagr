f621dab9d785120d6bc071c8ab40cc289e02b10a
Marius Bierlein <marius.bierlein@hey.car>
2020-05-15 19:09:51 +0200

    Refactor provider parameters

--- provider.go
+++ provider.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/mariusbierlein/terraform-provider-flagr/api"
+	"net/url"
 )
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"host": {
+			"api_host": {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"port": {
-				Type:     schema.TypeInt,
+			"api_path": {
+				Type:     schema.TypeString,
 				Optional: true,
-				Default:  80,
+				Default:  "/api/v1",
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
@@ -27,7 +27,9 @@ func Provider() *schema.Provider {
 		},
 		ConfigureFunc: func(data *schema.ResourceData) (i interface{}, err error) {
 			cfg := api.NewConfiguration()
-			cfg.BasePath = fmt.Sprintf("http://%v:%v/api/v1", data.Get("host"), data.Get("port"))
+			base, _ := url.Parse(data.Get("api_host").(string))
+			path, _ := url.Parse(data.Get("api_path").(string))
+			cfg.BasePath = base.ResolveReference(path).String()
 			client := api.NewAPIClient(cfg)
 
 			return client, nil
